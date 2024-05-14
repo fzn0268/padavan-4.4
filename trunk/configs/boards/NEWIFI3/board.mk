8e217659c4ca8653579fedad23b4f2115c95b7b4
edmund <11864905+fzn0268@users.noreply.github.com>
2024-05-14 23:40:07 +0000

    newifi3 single gmac support

--- trunk/configs/boards/NEWIFI3/board.mk
+++ trunk/configs/boards/NEWIFI3/board.mk
@@ -4,6 +4,9 @@
 # NEWIFI3   # NEWIFI3          # MT7621  #
 ##################################################################
 
-CFLAGS += -DBOARD_NEWIFI3
+CFLAGS += -DBOARD_NEWIFI3 -DUSE_SINGLE_MAC
 BOARD_NUM_USB_PORTS=1
 CONFIG_BOARD_RAM_SIZE=512
+ifdef CONFIG_RAETH_GMAC2
+unexport CONFIG_RAETH_GMAC2
+endif
